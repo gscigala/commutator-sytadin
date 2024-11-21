dedf1ddad245f1181dd54262389d33358ea62fe7
Guillaume Scigala <guillaume@scigala.fr>
2024-11-21 17:44:01 +0100

    Handle SIGINT signal

--- commutator_sytadin/main.py
+++ commutator_sytadin/main.py
@@ -1,15 +1,24 @@
 import sys
+import signal
 import dbus
 import dbus.mainloop.glib
 from gi.repository import GLib
 from .sytadin_dbus_service import SytadinDBusService
+
+def signal_handler(signum, frame):
+    print("SIGINT received, stopping the service...")
+    loop.quit()
 
 def main():
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
     bus_name = dbus.service.BusName('com.example.Sytadin', dbus.SessionBus())
     sytadin_service = SytadinDBusService(bus_name)
 
+    global loop
     loop = GLib.MainLoop()
+
+    signal.signal(signal.SIGINT, signal_handler)
+
     loop.run()
 
 if __name__ == "__main__":
